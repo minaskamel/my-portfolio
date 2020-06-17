f9fe1ecb687b1b0557ddff5ed0aefe75f5febb36
Minas Kamel <>
2020-06-18 00:29:21 +1000

    update portfolio code for lambda

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -1,19 +1,24 @@
+import json
 import boto3
 import StringIO
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3')
+def lambda_handler(event, context):
 
-portfolio_bucket = s3.Bucket('newportfolio.ausypt.com')
-build_bucket = s3.Bucket('portfoliocodebuild.ausypt.com')
-
-portfolio_zip = StringIO.StringIO()
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
-
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-       obj = myzip.open(nm)
-       portfolio_bucket.upload_fileobj(obj, nm,
-       ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-       portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+    s3 = boto3.resource('s3')
+    
+    portfolio_bucket = s3.Bucket('newportfolio.ausypt.com')
+    build_bucket = s3.Bucket('portfoliocodebuild.ausypt.com')
+    
+    portfolio_zip = StringIO.StringIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+           obj = myzip.open(nm)
+           portfolio_bucket.upload_fileobj(obj, nm,
+           ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+           portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
+           
+    return 'Hello From Lambda'
